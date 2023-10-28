e64125e7182e9022efc415ff637fa116dbdfda06
21120461 <88392096+Honghung123@users.noreply.github.com>
2023-10-28 21:41:57 +0700

    Tao giao dien cho trong web

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+<template>
+  <header class="header-container">
+    <div class="header-left">
+      <p class="text-left">Đoạn text bên trái</p>
+    </div>
+    <div class="header-center">
+      <h1>Tiêu đề trang</h1>
+    </div>
+    <div class="header-right">
+      <p class="text-right">Đoạn text bên phải</p>
+      <button class="btn btn-primary">Nút</button>
+    </div>
+  </header>
+</template>
+
+ 
